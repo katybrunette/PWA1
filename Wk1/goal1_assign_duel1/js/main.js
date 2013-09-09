1d0296145f8acb87b4d10130b4e017e932315a87
katybrunette <katybrunette@fullsail.edu>
2013-09-09 18:21:59 -0500

    First push/commit for assignment.

---
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,14 @@
+// Name: Katy Brunette
+// Date: 09/09/2013
+// Class: PWA 1 
+// Assignment: Goal 1 - Duel 1
+
+
+
+//self-executing function
+(function(){
+
+    console.log("FIGHT!!!");
+
+
+})();
