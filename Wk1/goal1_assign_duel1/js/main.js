49173214257533d6661d37f9c45b5bc4f8c78e3e
katybrunette <katybrunette@fullsail.edu>
2013-09-09 18:28:01 -0500

    Updated the code. Added the variables of player name, damage, and health.

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -10,5 +10,16 @@
 
     console.log("FIGHT!!!");
 
+    //player name
+    var playerOneName = "Spiderman";
+    var playerTwoName = "Batman";
+
+    //player damage
+    var player1Damage = 20;
+    var player2Damage = 20;
+
+    //player health
+    var playerOneHealth = 100;
+    var playerTwoHealth = 100;
 
 })();
