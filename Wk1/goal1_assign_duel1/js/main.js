4968b3d1c48313ffdc31e5e26d3a22a4f2b844d4
katybrunette <katybrunette@fullsail.edu>
2013-09-09 19:26:21 -0500

    Finished the assignment. Update to the final version of the code.

--- Wk1/goal1_assign_duel1/js/main.js
+++ Wk1/goal1_assign_duel1/js/main.js
@@ -22,24 +22,66 @@
     var playerOneHealth = 100;
     var playerTwoHealth = 100;
 
-    var round=1;
+    var round=0;
 
     function fight(){
         console.log('in the fight function');
 
-        for (var i=0; i<10; i++){
-            //code will go here
+        alert(playerOneName+":"+playerOneHealth+" *START* "+playerTwoName+":"+playerTwoHealth);
+
+        for (var i=0; i < 10; i++){
+            
+            var minDamage1 = player1Damage * .5;
+            var minDamage2 = player2Damage * .5;
+
+            var f1 = Math.floor(Math.random()*(player1Damage-minDamage1)+minDamage1);
+            var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
+
+        
+            //inflict damage
+            playerOneHealth-=f1;
+            playerTwoHealth-=f2;
+
+
+            console.log(playerOneName+":"+playerOneHealth+" "+playerTwoName+":"+playerTwoHealth);
+
+            var results = winnerCheck();
+            console.log(results);
+
+            if (results === "no winner"){
+                round++;
+                alert(playerOneName+":"+playerOneHealth+" *ROUND "+round+" OVER* "+playerTwoName+":"+playerTwoHealth);
+            }else{
+                alert(results);
+                break;
+            };
+
         };
+    
     };
 
-    function winnerCheck(){};
-        //code will go here
-    
+    function winnerCheck(){
+        console.log("in winnerCheck FN");
+
+        var result="no winner";
+
+        if (playerOneHealth<1 && playerTwoHealth<1){
+            result = "You Both Die :(";
+        } else if (playerOneHealth<1){
+            result = playerTwoName+" WINS!!!";
+        } else if (playerTwoHealth<1){
+            result = playerOneName+" WINS!!!";
+
+        };
+
+        return result;
+
+    };
+
 
     /******* The program gets started below *******/
     console.log('program starts');
     fight();
-
 
 
 })();
