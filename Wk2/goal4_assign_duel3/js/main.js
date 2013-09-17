f23b79ba9e7857753e236156abb97746ff4c99db
katybrunette <katybrunette@fullsail.edu>
2013-09-17 17:59:16 -0500

    Commented out alerts, replaced with the getElementById's.

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -1,5 +1,5 @@
 // Name: Katy Brunette
-// Date: 09/16/2013
+// Date: 09/17/2013
 // Class: PWA 1 
 // Assignment: Goal 4 - Duel 3
 // Instructor: Eric Silvay
@@ -11,8 +11,7 @@
     console.log("FIGHT!!!");
 
     //fighters object array
-    var fighters = [fighterOne = {
-        name:'Spiderman', damage:20, health:100}, fighterTwo = {name:'Batman', damage:20, health:100}];
+    var fighters = [{name:'Spiderman', damage:20, health:100}, {name:'Batman', damage:20, health:100}];
 
     var round=0;
 
@@ -20,20 +19,20 @@
         console.log('in the fight function');
 
         //alert(myArray[0]+":"+myArray[2]+" *START* "+myArray2[0]+":"+myArray2[2]);
-
+        console.log(document.getElementById());
 
 
         
             var minDamage1 = fighters[0].damage * .5;
-            var minDamage2 = fighters[1].damage * .5;
+            var minDamage2 = fighters[3].damage * .5;
 
             var f1 = Math.floor(Math.random()*(fighters[0].health-minDamage1)+minDamage1);
-            var f2 = Math.floor(Math.random()*(fighters[1].health-minDamage2)+minDamage2);
+            var f2 = Math.floor(Math.random()*(fighters[3].health-minDamage2)+minDamage2);
             console.log(minDamage1, minDamage2);
         
             //inflict damage
             fighters[0]-=f1;
-            fighters[1]-=f2;
+            fighters[3]-=f2;
 
 
             console.log(myArray[0]+":"+myArray[2]+" "+myArray2[0]+":"+myArray2[2]);
@@ -46,6 +45,7 @@
                 //alert(myArray[0]+":"+myArray[2]+" *ROUND "+round+" OVER* "+myArray2[0]+":"+myArray2[2]);
             }else{
                 //alert(results);
+                console.log(document.getElementById());
                 //break;
             };
 
